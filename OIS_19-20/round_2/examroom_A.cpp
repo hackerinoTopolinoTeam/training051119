a72a749c255e8912e55bd125a6d2a14a928a9cbf
mattia girolimetto <33939215+specialfish9@users.noreply.github.com>
2020-01-09 11:30:51 +0100

    Update examroom_A.cpp

--- OIS_19-20/round_2/examroom_A.cpp
+++ OIS_19-20/round_2/examroom_A.cpp
@@ -1,19 +1,10 @@
-/*
- * This template is valid both in C and in C++,
- * so you can expand it with code from both languages.
- */
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-// input data
 long long R, C, K;
 
 int main() {
-  //  uncomment the following lines if you want to read/write from files
-  //  freopen("input.txt", "r", stdin);
-  //  freopen("output.txt", "w", stdout);
 
   assert(3 == scanf("%lld %lld %lld", &R, &C, &K));
 
@@ -21,8 +12,7 @@ int main() {
 
   long long risR = (R %K == 0)?R/K:R/K +1;
   long long result = risC * risR;
-  // insert your code here
-
+  
   printf("%lld\n", result);  // print the result
   return 0;
 }
